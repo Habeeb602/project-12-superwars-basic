3a80262c1caa6f74c5fbb99824f5ca776e4ec134
Habeeb602 <jhabeeb602@gmail.com>
2021-10-02 14:35:30 +0530

    Habeeb Rahman J (20MCA1025) Completed

--- src/app.js
+++ src/app.js
@@ -26,7 +26,20 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+    for(let i=0;i<PLAYERS.length;i++){
+        let player={
+            "name":PLAYERS[i],
+            "strength":getRandomStrength(),
+            "image":"images/super-"+(i+1)+".png"
+        }
+        if(i%2==0){
+            player["type"]="hero";
+        }
+        else{
+            player["type"]="villain";
+        }
+        detailedPlayers.push(player);
+    }
     return detailedPlayers;
 }
 
@@ -34,6 +47,7 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.round(Math.random() * (100-0) + 0);
 }
 
 const buildPlayers = (players, type) => {
@@ -42,6 +56,30 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    if(type=="hero"){
+        for(const k of players){
+            if(k['type']=="hero"){
+                fragment+=`
+                <div class="player">
+                    <img src=${k["image"]} alt="">
+                    <div class="name">${k["name"]}</div>
+                    <div class="strength">${k["strength"]}</div>
+                </div>`
+            }
+        }
+    }
+    else{
+        for(const k of players){
+            if(k['type']=="villain"){
+                fragment+=`
+                <div class="player">
+                    <img src=${k["image"]}>
+                    <div class="name">${k["name"]}</div>
+                    <div class="strength">${k["strength"]}</div>
+                </div>`
+            }
+        }
+    }
 
     return fragment;
 }
